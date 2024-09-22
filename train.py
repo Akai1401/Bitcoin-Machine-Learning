d439d1aadb316a066a641ee31002e92e168a149b
Akai <huy123nn69hz@gmail.com>
2024-09-22 23:08:03 +0700

    ok

--- train.py
+++ train.py
@@ -14,10 +14,11 @@ df.dropna(inplace=True)
 X = df[['total_volume', 'market_cap']]
 y = df['price']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
+X_validation, X_test, y_validation, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
 # Lasso Model
-lasso_model = Lasso()
+lasso_model = Lasso(alpha=1.0)
 lasso_model.fit(X_train, y_train)
 joblib.dump(lasso_model, 'models/lasso_model.pkl')
 
@@ -26,7 +27,7 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-neural_network_model = MLPRegressor(hidden_layer_sizes=(50, 20), activation='relu', solver='adam', alpha=1.0, learning_rate_init=0.001, max_iter=500, random_state=42)
+neural_network_model = MLPRegressor(hidden_layer_sizes=(50, 50), activation='relu', solver='lbfgs', alpha=0.0001, max_iter=1000, random_state=42)
 neural_network_model.fit(X_train_scaled, y_train)
 joblib.dump((neural_network_model, scaler), 'models/neural_network_model.pkl')
 
